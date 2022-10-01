3f199fe1449691f04c0485dff949e864071a22c1
Benjamin Bose <>
2022-10-01 21:17:24 +0200

    changed Mhalo to Mvir

--- reactions/src/BeyondLCDM.cpp
+++ reactions/src/BeyondLCDM.cpp
@@ -1274,7 +1274,7 @@ double gamma3(double a, double omega0, double k0, double k1, double k2, double k
 double mymgF(double a, double yh, double yenv, double Rth, double omega0, double extpars[], double delta_initial, int model){
 	double h0 = 1./2997.92;
 	double dod, dod2, dRRth, fr0, var1, var2, term1, term2, term3;
-	double betadgp,xm3,xterm,delta,Mhalo;
+	double betadgp,xm3,xterm,delta,Mvir;
 //	double alphaofa[5],dalphaofa[5],lambda2; //EFTofDE
 	switch(model) {
 	  case 1:
@@ -1328,9 +1328,9 @@ double mymgF(double a, double yh, double yenv, double Rth, double omega0, double
 			// 5-12 : p1-p8
 
 					var1 = extpars[5]/(extpars[5]-1.)*extpars[7]; // a
-					Mhalo = pow3(Rth/0.1)*5.*omega0/Gnewton; // halo mass x Gnewton - see definition in scol_init in HALO.cpp
+					Mvir = pow3(Rth/0.1)*5.*omega0/Gnewton; // virial mass x Gnewton - see definition in scol_init in HALO.cpp
 
-					term1 = extpars[8]*pow(a,extpars[9]) * pow(2.*Gnewton*h0*Mhalo/pow2(sofl),extpars[10])*pow(yenv/yh,extpars[11]); // y0
+					term1 = extpars[8]*pow(a,extpars[9]) * pow(2.*Gnewton*h0*Mvir/pow2(sofl),extpars[10])*pow(yenv/yh,extpars[11]); // y0
 
 					xm3 = pow(term1/yh,var1); // (y0/yh)^a
 
@@ -1360,9 +1360,9 @@ double mymgF(double a, double yh, double yenv, double Rth, double omega0, double
 			// 5-12 : p1-p8
 
 					var1 = extpars[5]/(extpars[5]-1.)*extpars[7]; // a
-					Mhalo = pow3(Rth/0.1)*5.*omega0/Gnewton; // halo mass x Gnewton - see definition in scol_init in HALO.cpp
+					Mvir = pow3(Rth/0.1)*5.*omega0/Gnewton; // virial mass x Gnewton - see definition in scol_init in vir.cpp
 
-					term1 = extpars[8]*pow(a,extpars[9]) * pow(2.*Gnewton*h0*Mhalo/pow2(sofl),extpars[10])*pow(yenv/yh,extpars[11]); // y0
+					term1 = extpars[8]*pow(a,extpars[9]) * pow(2.*Gnewton*h0*Mvir/pow2(sofl),extpars[10])*pow(yenv/yh,extpars[11]); // y0
 
 					xm3 = pow(term1/yh,var1); // (y0/yh)^a
 
